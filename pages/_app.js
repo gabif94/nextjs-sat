5496029b70a9389861021aa0f41f75791262f25a
Gabriel Fresco <gabi94f@gmail.com>
2022-03-03 16:54:42 -0300

    adding chakra ui config

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,41 @@
-import {AuthProvider} from '../lib/auth';
-import '../styles/globals.css';
+import {ChakraProvider} from '@chakra-ui/react';
+import {AuthProvider} from '@/lib/auth';
+import theme from '@/styles/theme';
+import {css, Global} from '@emotion/react';
+import Head from 'next/head';
+
+const GlobalStyle = ({children}) => {
+	return (
+		<>
+			<Head>
+				<meta content="width=device-width, initial-scale=1" name="viewport" />
+			</Head>
+
+			<Global
+				styles={css`
+					html {
+						scroll-behavior: smooth;
+					}
+					#__next {
+						display: flex;
+						flex-direction: column;
+						min-height: 100vh;
+					}
+				`}
+			/>
+			{children}
+		</>
+	);
+};
 
 function MyApp({Component, pageProps}) {
 	return (
-		<AuthProvider>
-			<Component {...pageProps} />
-		</AuthProvider>
+		<ChakraProvider theme={theme}>
+			<AuthProvider>
+				<GlobalStyle />
+				<Component {...pageProps} />
+			</AuthProvider>
+		</ChakraProvider>
 	);
 }
 
